64eae2aeb360cc73ab4f60899513f1ba74a3233d
Deyu Fu <deyuf@nvidia.com>
2016-12-13 13:10:43 -0800

    reserve CNTK_MODEL_VERSION_17 for dilated convolution

--- Source/ComputationNetworkLib/ConvolutionalNodes.h
+++ Source/ComputationNetworkLib/ConvolutionalNodes.h
@@ -290,6 +290,7 @@ public:
     {
         Base::Save(fstream);
         fstream << m_convolution2D;
+        TensorShape(1).Save(fstream); // Write out a dummy tensor, so that model created can be used later after implementing reading this tensor in this model version
     }
 
     void Load(File& fstream, size_t modelVersion) override
